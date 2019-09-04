3b7bdb9df6223fb61407757a2fd8be824056f320
ArthurKa <arthur.katruk@gmail.com>
2019-09-04 08:56:17 +0300

    Minor changes
    Signed-off-by: ArthurKa <arthur.katruk@gmail.com>

--- declineWord.js
+++ declineWord.js
@@ -1,17 +1,19 @@
 'use strict';
 
-function declineWord(n, body, one = '', two_four = '', zero_or_five_nine = '') {
+function declineWord(n, body, one = '', two_till_four = '', zero_or_five_till_nine = '') {
   n = Math.abs(n % 100);
-  const n1 = Math.floor(n/10);
+  const n1 = Math.floor(n / 10);
   const n2 = n % 10;
 
-  if(n1 === 1 || [0, 5, 6, 7, 8, 9].includes(n2)) {
-    return body + zero_or_five_nine;
+  if(n1 !== 1) {
+    if(n2 === 1) {
+      return `${body}${one}`;
+    }
+    if(2 <= n && n <= 4) {
+      return `${body}${two_till_four}`;
+    }
   }
-  if(n2 === 1) {
-    return body + one;
-  }
-  return body + two_four;
+  return `${body}${zero_or_five_till_nine}`;
 }
 
 module.exports = declineWord;
