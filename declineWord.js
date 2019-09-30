f7360b5ff0fd8e6ab70071736169330f97af62ed
ArthurKa <arthur.katruk@gmail.com>
2019-09-30 04:21:52 +0300

    English words autoending added
    Signed-off-by: ArthurKa <arthur.katruk@gmail.com>

--- declineWord.js
+++ declineWord.js
@@ -1,9 +1,19 @@
 'use strict';
 
-function declineWord(n, body, one = '', two_till_four = '', zero_or_five_till_nine = '') {
+function declineWord(n, body, one = null, two_till_four = null, zero_or_five_till_nine = null) {
   n = Math.abs(n % 100);
   const n1 = Math.floor(n / 10);
   const n2 = n % 10;
+
+  if(!(body || one).match(/[a-z]/i)) {
+    one = one || '';
+    two_till_four = two_till_four || '';
+    zero_or_five_till_nine = zero_or_five_till_nine || '';
+  } else {
+    one = one != null ? one : '';
+    two_till_four = two_till_four != null ? two_till_four : 's';
+    zero_or_five_till_nine = zero_or_five_till_nine != null ? zero_or_five_till_nine : two_till_four;
+  }
 
   if(n1 !== 1) {
     if(n2 === 1) {
