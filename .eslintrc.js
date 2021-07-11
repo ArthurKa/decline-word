0337309476ef795e952ccc046abd7b102473fdbf
ArthurKa <arthur.katruk@gmail.com>
2021-07-11 23:30:03 +0300

    feat: add @arthurka/eslint package

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+// @ts-check
+'use strict';
+
+/** @type { import('eslint').Linter.Config } */
+module.exports = {
+  extends: './node_modules/@arthurka/eslint',
+  env: {
+    mocha: true,
+  },
+};
--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,12 @@ export default function declineWord(
   endingForTwo?: string,
   endingForFive?: string,
 ) {
+  // eslint-disable-next-line no-param-reassign
   n = Math.abs(n % 100);
   const n1 = Math.floor(n / 10);
   const n2 = n % 10;
 
+  /* eslint-disable no-param-reassign */
   if(!(mainPart || endingForOne || '').match(/[a-z]/i)) {
     endingForOne = endingForOne || '';
     endingForTwo = endingForTwo || '';
@@ -16,14 +18,15 @@ export default function declineWord(
   } else {
     endingForOne = endingForOne != null ? endingForOne : '';
     endingForTwo = endingForTwo != null ? endingForTwo : 's';
-    endingForFive = endingForFive != null ? endingForFive : endingForTwo;
   }
+  /* eslint-enasble no-param-reassign */
 
+  endingForFive = endingForFive != null ? endingForFive : endingForTwo;
   if(n1 !== 1) {
     if(n2 === 1) {
       return `${mainPart}${endingForOne}`;
     }
-    if(2 <= n2 && n2 <= 4) {
+    if(n2 >= 2 && n2 <= 4) {
       return `${mainPart}${endingForTwo}`;
     }
   }
